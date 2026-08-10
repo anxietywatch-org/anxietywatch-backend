77c538f6a9f3ed873c19917a439172811c6d49ce
anxietywatch483-cyber <anxietywatch483-cyber@users.noreply.github.com>
2026-08-10 00:11:43 -0600

    test: disable system event log in integration host

--- tests/AnxietyWatch.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
+++ tests/AnxietyWatch.IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AnxietyWatch.IntegrationTests.Infrastructure;
 
@@ -9,6 +10,7 @@ public sealed class CustomWebApplicationFactory : WebApplicationFactory<Program>
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Testing");
+        builder.ConfigureLogging(logging => logging.ClearProviders());
         builder.ConfigureAppConfiguration((_, configuration) =>
         {
             configuration.AddInMemoryCollection(new Dictionary<string, string?>
